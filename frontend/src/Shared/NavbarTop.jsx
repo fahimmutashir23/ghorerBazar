d1bc9cbbc86a8f02002dd9431f6cca494369254e
MD FAHIM MUNTASHIR <taslimit23@gmail.com>
2024-12-30 00:41:55 +0630

    fix

--- frontend/src/Shared/NavbarTop.jsx
+++ frontend/src/Shared/NavbarTop.jsx
@@ -4,7 +4,7 @@ const NavbarTop = () => {
     <div className="bg-color_1 py-3">
       <div className="">
         <p className="text-white font-medium text-sm lg:text-xl text-center">
-        আমাদের যে কোন পণ্য অর্ডার করতে কল বা WhatsApp করুন:  +8801321208940 | হট লাইন: 09642-922922
+        আমাদের যে কোন পণ্য অর্ডার করতে কল বা WhatsApp করুন:  +880 1729-798079 | ই-মেইল: ghuribazarbd@gmail.com
         </p>
       </div>
     </div>
